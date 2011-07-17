30c5158a94069dfac87ef814be448207d4710095
Jason Salaz <jason@zenenet.com>
2011-07-17 16:19:23 -0600

    Scripts for parsing appropriate data out of MPEG-4 (mp4/m4a) Audio files, and MPEG-1 Layer 3 (mp3) Audio files. First README for Github.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Musicbrainz Track Parsers #
+
+## Requirements ##
+
+* [Audio::Scan](http://search.cpan.org/~agrundma/Audio-Scan-0.87/lib/Audio/Scan.pm) from CPAN.
+* Perl. I'm not going to explain how to get it.
+
+## Usage ##
+
+Invoke a file format script suitable for the media in your files, pipe to sort if necessary.
+
+## Roadmap / Wishlist ##
+
+* Suitable framework for detecting file-formats, both dumbly (by extension) and smartly (by file magic/mimetype), turning this into just a single script.
+* Seemless `pbcopy` integration into OS X environments.
+* Smart artist display, automatically when "Various Artists" is seen, or when differing artists are detected on tracks.
---
+++ readM4aTags.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use Audio::Scan qr/scan/;
+local $ENV{AUDIO_SCAN_NO_ARTWORK} = 1;
+
+sub convertMsToTime
+{
+	my $min = 0;
+	my $sec = 0;
+
+	my $ms = shift;
+	while($ms > 60000)
+	{
+		$ms -= 60000;
+		$min++;
+	}
+	while($ms > 1000)
+	{
+		$ms -= 1000;
+		$sec++;
+	}
+	
+	# Ensure there's a leading 0 on the number of seconds.
+	$sec = sprintf("%02d", $sec);
+	
+	# Return value formatted MM:SS. Cute :).
+	return "${min}:${sec}";
+}
+
+opendir(my $dh, $ARGV[0]) || die "What a boob. Directory FAIL! $!";
+my @files = grep { /\.m4a/ } readdir($dh);
+
+my ($tags, $length, $trackNr, $artist, $title, $album);
+my $i=1;
+	
+foreach(@files)
+{
+	$tags = Audio::Scan->scan("$ARGV[0]/$_");
+	$length = convertMsToTime(${tags}->{'info'}->{'song_length_ms'});
+
+	$trackNr = ${tags}->{'tags'}->{'TRKN'} || $i;
+	$trackNr =~ s{/.*}{};
+	$title = ${tags}->{'tags'}->{'NAM'} || "Unknown Title";
+	$artist = ${tags}->{'tags'}->{'ART'} || "Unknown Artist";
+	
+	print "${trackNr}.	${title} - ${artist}	${length}\n";
+	
+	$i++;
+}
+
+closedir($dh);
